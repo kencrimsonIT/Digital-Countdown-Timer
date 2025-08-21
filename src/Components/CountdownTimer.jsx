49e848ab97d40599b7eac6ad403fcce5ba6f4fd5
kencrimsonIT <tongkien930@gmail.com>
2025-08-21 22:00:34 +0700

    Initial commit

---
+++ src/Components/CountdownTimer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TimeUnit from "../Components/TimeUnit.jsx";
+import "../CSS/CountdownTimer.css";
+
+const CountdownTimer = () => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default CountdownTimer;
---
+++ src/Components/TimeUnit.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../CSS/TimeUnit.css";
+
+const TimeUnit = () => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default TimeUnit;
