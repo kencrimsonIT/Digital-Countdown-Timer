eaf3059c7cda5d2086fe490e979b7159ea4734d4
kencrimsonIT <tongkien930@gmail.com>
2025-08-22 10:57:29 +0700

    Import timer & GlobalStyle component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import CountdownTimer from './Components/CountdownTimer.jsx';
+import {createGlobalStyle} from "styled-components";
 import './App.css';
+
+const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Orbitron:wght@400..900&display=swap');
+    
+    body {
+        font-family: "Orbitron", sans-serif;
+    }
+`;
 
 function App() {
   return (
-    <div>
-
+    <div className="timer">
+        <GlobalStyle />
+        <CountdownTimer initialSeconds={86400} />
     </div>
   );
 }
