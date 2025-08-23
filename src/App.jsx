23ab28992f17cb20d777793b92ada90fc96d2469
kencrimsonIT <tongkien930@gmail.com>
2025-08-23 12:06:02 +0700

    Set default 'initialSeconds' as 0

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   return (
     <div className="timer">
         <GlobalStyle />
-        <CountdownTimer initialSeconds={86400} />
+        <CountdownTimer initialSeconds={0} />
     </div>
   );
 }
