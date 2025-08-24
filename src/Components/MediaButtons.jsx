9dbd4dde7af211939879b3a1bacbcf385e782740
kencrimsonIT <tongkien930@gmail.com>
2025-08-24 10:43:35 +0700

    FIX #4: Change 'onPLay' into 'onPlay'

--- src/Components/MediaButtons.jsx
+++ src/Components/MediaButtons.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "../CSS/MediaButtons.css";
 
-export const MediaButtons = ({onPLay, onPause, onStop}) => {
+export const MediaButtons = ({onPlay, onPause, onStop}) => {
     return (
         <>
-            <button className="play-btn" onClick={onPLay} aria-label="Play">&#9654;</button>
+            <button className="play-btn" onClick={onPlay} aria-label="Play">&#9654;</button>
             <button className="stop-btn" onClick={onStop} aria-label="Stop">&#9632;</button>
             <button className="pause-btn" onClick={onPause} aria-label="Pause">&#9208;</button>
         </>
